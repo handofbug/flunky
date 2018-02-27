a01e9f1aedcd25abf09a3f3eabbb7bb860707eb5
Ange Near <handofbug@gmail.com>
2018-02-27 20:30:27 +0300

    add module load

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let config = require('config-yml');
+const path = require('path');
+const fs = require('fs');
+const config = require('config-yml');
+const modules = {};
+const settings = require('./config/settings.json');
 
-
-let settings = require('./config/settings.json');
 //If dev, use local config.yml settings
 settings.token = (process.env.NODE_ENV === "dev" ? config.token : settings.token);
 
+fs.readdirSync(path.join(__dirname, "modules")).forEach(file => {
+    const module = require('./modules/' + file);
+    modules[file] = module;
+});
 
 
 
 
 console.log(settings);
+console.log(modules);
