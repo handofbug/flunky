1cd5193153025626749460a9f399e573cc5f3749
Ange Near <handofbug@gmail.com>
2018-02-28 18:52:06 +0300

    module arch

--- modules/test.js
+++ modules/test.js
@@ -2,6 +2,7 @@ module.exports = {
     textActions: [{
         description: 'Just a hello',
         pattern: /hi/gi,
+        
         func: (message) => {
             console.log(this.textActions);
             message.channel.send('hello to u');
